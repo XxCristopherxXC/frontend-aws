58c2fab6988f4764e70a25d22c6d45c01c4f7af9
XxCristopherxXC <cr.barrueto@duocuc.cl>
2026-05-12 11:07:01 +0000

    Configuración Docker inicial - Frontend

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <h1>¡Éxito! Frontend Dockerizado en Amazon Linux 2023</h1>
+)
